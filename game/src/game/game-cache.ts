b2c51e06ba869a262ab7765705c6c05121fccf9a
Steffan Sølvsten <201505832@post.au.dk>
2025-11-09 10:55:20 +0100

    Use last word instead of faulty hash
    The last word in each game is guaranteed to be unique. Hence, it is a much
    more fail-safe way to notice whether the game in the cache and the one being
    loaded differ.
    
    Changing this broke the game, as the values in local storage did not have the
    expected values.

--- game/src/game/game-cache.ts
+++ game/src/game/game-cache.ts
@@ -1,7 +1,21 @@
 import { Language } from "../language";
 import { Mode } from "../mode";
 
-const dailyCache: { [lang in Language]: { hash: number, words: string[] } | undefined} = {
+class CacheEntry {
+    key: string;
+    guessed: string[];
+
+    constructor(key: string, guessed: string[]) {
+        this.key = key;
+        this.guessed = guessed
+    }
+};
+
+function isCacheEntry (obj: any) : obj is CacheEntry {
+    return obj && obj.key && obj.guessed;
+}
+
+const dailyCache: { [lang in Language]: CacheEntry | undefined} = {
     [Language.DK]: undefined,
     [Language.DE]: undefined,
     [Language.EN]: undefined,
@@ -10,18 +24,34 @@ const dailyCache: { [lang in Language]: { hash: number, words: string[] } | unde
 
 const getLSKey = (mode: Mode, language: Language) => `${mode}:${language}`;
 
-/** Setup the cache for a certain game mode and language. This flushes the cache if the hash of the
- *  words change. */
+/** Setup the cache for a certain game mode and language. This flushes the cache if the game is a
+ *  different one than the one that was cached. */
 export const setup = (mode: Mode, language: Language, words: string[] | undefined) => {
     if (mode !== Mode.DAILY || !words) { return; }
 
-    const hash = words.length*1024 + words.reduce((x, y) => x + y.length, 0);
-    const empty = { hash, words: [] };
+    const key = words[words.length-1];
+    const empty = new CacheEntry(key, []);
 
     let languageCache = dailyCache[language];
-    if (!languageCache || languageCache.hash !== hash) {
-        const ls_res = localStorage.getItem(getLSKey(mode, language));
-        dailyCache[language] = ls_res ? JSON.parse(ls_res) : empty;
+    if (!languageCache || languageCache.key !== key) {
+        // Check local storage for a value.
+        const ls_key = getLSKey(mode, language);
+        const ls_res = localStorage.getItem(ls_key);
+        if (!ls_res) {
+            dailyCache[language] = empty;
+            return;
+        }
+        // Check whether the value is old (does not match `CacheEntry`) and should be ignored. This
+        // resolves the webpage from breaking when the local storage contains unmigrated data.
+        const ls_obj = JSON.parse(ls_res);
+        if (!isCacheEntry(ls_obj)) {
+            console.log(`purging ${ls_res} from cache`);
+            localStorage.removeItem(ls_key);
+            dailyCache[language] = empty;
+            return;
+        }
+        // Use the value from storage.
+        dailyCache[language] = JSON.parse(ls_res);
     }
 };
 
@@ -31,13 +61,13 @@ export const get = (mode: Mode, language: Language) => {
 
     const languageCache = dailyCache[language];
     if (!languageCache) { return []; }
-    return languageCache.words;
+    return languageCache.guessed;
 }
 
 /** Cache a guessed word for a given game mode and language. */
 export const push = (mode: Mode, language: Language, word: string) => {
     if (mode !== Mode.DAILY) { return; }
 
-    dailyCache[language]?.words.push(word);
+    dailyCache[language]?.guessed.push(word);
     localStorage.setItem(getLSKey(mode, language), JSON.stringify(dailyCache[language]));
 }
